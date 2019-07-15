535aa426184d42a46bf95873cde2826f5d8632cb
Paul Holden <pholden@stria.com>
2019-07-15 12:23:18 -0700

    Capture connection info

--- lib/springcm/client.rb
+++ lib/springcm/client.rb
@@ -6,6 +6,10 @@ module Springcm
       if !['na11', 'uatna11'].include?(data_center)
         raise Springcm::ConnectionInfoError.new("Invalid data center '#{data_center.to_s}'")
       end
+
+      @data_center = data_center
+      @client_id = client_id
+      @client_secret = client_secret
     end
   end
 end
