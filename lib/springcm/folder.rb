aa95e0106fbe86bd1026741c3c33e77d8efadba3
Paul Holden <paul@holdensoftware.com>
2019-07-19 14:09:07 -0700

    Add some documentation comments
    [ci skip]

--- lib/springcm/folder.rb
+++ lib/springcm/folder.rb
@@ -5,26 +5,32 @@ module Springcm
       @data = data
     end
 
+    # @return [Boolean] Does the API user have see permission
     def see?
       !!access_level.dig("See")
     end
 
+    # @return [Boolean] Does the API user have read permission
     def read?
       !!access_level.dig("Read")
     end
 
+    # @return [Boolean] Does the API user have write permission
     def write?
       !!access_level.dig("Write")
     end
 
+    # @return [Boolean] Does the API user have move permission
     def move?
       !!access_level.dig("Move")
     end
 
+    # @return [Boolean] Does the API user have create permission
     def create?
       !!access_level.dig("Create")
     end
 
+    # @return [Boolean] Does the API user have set access permission
     def set_access?
       !!access_level.dig("SetAccess")
     end
