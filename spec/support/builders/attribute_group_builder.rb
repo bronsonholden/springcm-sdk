27d4cc0d547846435f251376053ddf446de73cf3
Paul Holden <paul@holdensoftware.com>
2019-12-31 11:18:07 -0800

    Add IsSystem field to AttributeGroup builder data result

--- spec/support/builders/attribute_group_builder.rb
+++ spec/support/builders/attribute_group_builder.rb
@@ -14,6 +14,7 @@ class AttributeGroupBuilder < Builder
   def data
     {
       "Name" => name,
+      "IsSystem" => false,
       "Attributes" => attributes,
       "Href" => "#{client.object_api_url}/attributegroups/#{uid}"
     }
