f4f6957a30db005a15af9cb8e73c300f5cf6f82e
Paul Holden <paul@holdensoftware.com>
2020-01-09 16:31:26 -0800

    Raise error in #await!, return true/false in #await

--- lib/springcm-sdk/change_security_task.rb
+++ lib/springcm-sdk/change_security_task.rb
@@ -2,14 +2,22 @@ require "springcm-sdk/resource"
 
 module Springcm
   class ChangeSecurityTask < Resource
-    def await(interval: 1, tries: 10, backoff: 2)
+    def await!(interval: 1, tries: 10, backoff: 2)
       while tries > 0
-        return if complete?
+        return true if complete?
         sleep(interval)
         interval *= backoff
         tries -= 1
       end
       raise Springcm::ChangeSecurityTaskAwaitTimeout.new
+    end
+
+    def await(interval: 1, tries: 10, backoff: 2)
+      begin
+        await!(interval: interval, tries: tries, backoff: backoff)
+      rescue Springcm::ChangeSecurityTaskAwaitTimeout => timeout
+        return false
+      end
     end
 
     def complete?
